6fd7550724114bb614358dc4af7e5d1fb40560db
boblaublaw <github@joeboyle.com>
2019-11-04 11:49:40 -0800

    fixed ambiuous err var, added hash init and insertion

--- godedup.go
+++ godedup.go
@@ -13,14 +13,15 @@ type analyzer struct {
 	hashmap map[string]string
 }
 
-func (* analyzer) processtree(path string, entry os.FileInfo, err error) error {
+func (a analyzer) processtree(path string, entry os.FileInfo, err error) error {
 	if err != nil {
 	    return err
 	}
 	if entry.IsDir() {
-		fmt.Println("dir " + path, entry.Size())
+		//fmt.Println("dir " + path, entry.Size())
 	} else {
-		fmt.Println("file " + path, entry.Size())
+		//fmt.Println("file " + path, entry.Size())
+		a.pathmap[path] = "woo"
 	}
 	return nil	
 }
@@ -28,29 +29,33 @@ func (* analyzer) processtree(path string, entry os.FileInfo, err error) error {
 func NewAnalyzer(paths []string) (*analyzer, error) {
 	a := analyzer{paths: paths}
 
-	for _, element := range paths {
-		var err error
+	a.pathmap = make(map[string]string)
+	a.hashmap = make(map[string]string)
 
+	for _, element := range paths {
 		processfunc := func(path string, fileInfo os.FileInfo, e error) (err error) {
-			err = a.processtree(path, fileInfo, e)
-			if err != nil {
+			e = a.processtree(path, fileInfo, e)
+			if e != nil {
 			    log.Println(err)
-			    return err
+			    return e
 			}
 			return nil
 		}
-		err = filepath.Walk(element, processfunc)
+		err := filepath.Walk(element, processfunc)
 		if err != nil {
 		    log.Println(err)
 		    return nil, err
 		}
-
 	}
+	// do more synthesis here
+	fmt.Println(a)
 	return &a, nil
 }
 
 func main() {
-	a, err := NewAnalyzer(os.Args[1:])
+	var a *analyzer
+	var err error
+	a, err = NewAnalyzer(os.Args[1:])
 	if err != nil {
 	    log.Println(err)
 	}

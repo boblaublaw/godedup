1fe1520c71ddffa7cefc2b2bbb6b73fff0c19b50
boblaublaw <github@joeboyle.com>
2019-11-04 19:21:55 -0800

    tree traversal working but very noisy

--- godedup.go
+++ godedup.go
@@ -5,61 +5,126 @@ import (
     "io"
     "os"
     "log"
-    "sort"
+    //"sort"
     "errors"
+    "bytes"
     "strings"
     "path/filepath"
     "crypto/sha1"
-    "encoding/hex"
+    //"encoding/hex"
 )
+
+type File struct {
+    Name string
+}
+
+type Dir struct {
+    Name  string
+    Files []File
+    Dirs  map[string]*Dir
+}
+
+func canonicalpath(pathname string) string {
+	var buf bytes.Buffer
+	var last rune
+	for i, r := range pathname {
+		if r != last || i == 0 || r != os.PathSeparator {
+			buf.WriteRune(r)
+			last = r
+		}
+	}
+	pathname = buf.String()
+	if (pathname[:2] == "./"){
+		pathname=pathname[2:]
+	}
+	return pathname
+}
+
+func newDir(name string) *Dir {
+	fmt.Println("creating new dir: " + name)
+	d := Dir{name, []File{}, make(map[string]*Dir)}
+    return &d
+}
+
+func (f *Dir) addDir(path []string) (error) {
+	fmt.Printf("addDir on \"%s\"",strings.Join(path, "/"))
+	fmt.Printf(" with len %d\n", len(path))
+
+	next := path[0]
+	rem := path[1:]
+
+	fmt.Println("placing " + next + " in " + f.Name)
+	if val, ok := f.Dirs[next]; ok {
+		fmt.Println("found " + next + " in " + f.Name)
+		return (val.addDir(rem))
+	}
+	fmt.Println("couldnt find " + next + " in " + f.Name)
+	f.Dirs[next] = newDir(next)
+	if len(rem) > 1 {
+		fmt.Println("moving on to " + strings.Join(rem, "/") + " in " + next)
+		return (f.Dirs[next].addDir(rem))
+	}
+	// directory already exists, do nothing
+	return nil
+}
+
+func (d *Dir) addFile(path []string) error {
+	//fmt.Printf("adding file %s\n", strings.Join(path, "/") )
+	next := path[0]
+	rem := path[1:]
+	if len(rem) == 0 {
+		// this is the file
+		d.Files = append(d.Files, File{next})
+		return nil
+	}	
+	// still finding the containing dir
+	if val, ok := d.Dirs[next]; ok {
+		return (val.addFile(rem))
+	}
+	return errors.New("somehow can't find a subdir for file placement")
+}
+
+func (currdir *Dir) Dump(preface string) {
+    for _, file := range currdir.Files {
+        fmt.Printf("%s%s%c%s\n", preface, currdir.Name, os.PathSeparator, file.Name)
+    }
+    for _, subdir := range currdir.Dirs {
+        subdir.Dump(preface + currdir.Name + string(os.PathSeparator))
+    }
+    fmt.Printf("%s%s\n", preface, currdir.Name)
+}
 
 // top level object so nothing is global
 type analyzer struct {
-	requestedpaths []string
-	filemap map[string]string
-	filesbylen []string
-	dirmap map[string]string
-	dirsbylen []string
+	rootdirs       []*Dir
 }
 
 // creates new analyzer object and starts populating it based on requested paths
-func NewAnalyzer(paths []string) (*analyzer, error) {
+func NewAnalyzer(rootpaths []string) (*analyzer, error) {
 	a := analyzer {
-		requestedpaths:paths,
-		filemap:make(map[string]string),
-		filesbylen:make([]string,0),
-		dirmap:make(map[string]string),
-		dirsbylen:make([]string,0),
+		rootdirs:make([]*Dir,0),
 	}
 	var err error
-	for _, element := range paths {
-		fileclosure := func(path string, fileInfo os.FileInfo, e error) (err error) {
+	for _, rootpathname := range rootpaths {
+		// fold any duplicate slashes down to just one
+		rootpathname = canonicalpath(rootpathname)
+		// add this dir to the requested top level dirs
+		a.rootdirs = append(a.rootdirs, newDir(rootpathname))
+
+		processclosure := func(path string, fileInfo os.FileInfo, e error) (err error) {
 			e = a.process(path, fileInfo, e)
 			if e != nil {
-			    log.Println(err)
-			    return e
+			    log.Println("error in process closure:")
+			    log.Println(e)
+			    os.Exit(-1)
 			}
 			return nil
 		}
-		err = filepath.Walk(element, fileclosure)
+		err = filepath.Walk(rootpathname, processclosure)
 		if err != nil {
 		    log.Println(err)
 		    return nil, err
 		}
-	}
-	for k, _ := range a.filemap {
-        a.filesbylen = append(a.filesbylen, k)
-    }
-    for k, _ := range a.dirmap {
-        a.dirsbylen = append(a.dirsbylen, k)
-    }
-    // sort the dir and file names by length, longest first:
-    sort.Slice(a.filesbylen, func(i, j int) bool { return len(a.filesbylen[i]) > len(a.filesbylen[j]) })
-    sort.Slice(a.dirsbylen, func(i, j int) bool { return len(a.dirsbylen[i]) > len(a.dirsbylen[j]) })
-	err = a.hashdirs()
-	if err != nil {
-		log.Println(err)
-		return nil, err
 	}
 	return &a, nil
 }
@@ -67,26 +132,11 @@ func NewAnalyzer(paths []string) (*analyzer, error) {
 // hashes a specific directory based on its contents
 func (a analyzer) hashdir (dirname string) (error) {
 	// find all the files and dirs directly contained herein
-
-	dirnamelen := len(dirname)
-	for filename, _ := range a.filemap {
-		filenamelen := len(filename)
-		if dirnamelen >= filenamelen {
-			break
-		}
-		fmt.Println(filename + " may be a member of " + dirname)
-	}
-	// close the hash here
-	fmt.Println("DIR:  " + dirname)
 	return nil
 }
 
 // hashes all directories, starting with the longest first
 func (a analyzer) hashdirs () (error) {
-
-	for _, k := range a.dirsbylen {
-		a.hashdir(k)
-	}
 	return nil
 }
 
@@ -101,31 +151,39 @@ func (a analyzer) hashfile(path string) (error) {
 	if _, err := io.Copy(h, f); err != nil {
 		return err
 	}
-	a.filemap[path] = hex.EncodeToString(h.Sum(nil))
+	//a.filemap[path] = hex.EncodeToString(h.Sum(nil))
 	return nil
 }
 
 // the callback from filepath.walk to process both dirs and files
 func (a analyzer) process(path string, entry os.FileInfo, err error) error {
 	if err != nil {
+		fmt.Println("process being called with an error")
+		fmt.Println(err)
 	    return err
 	}
-	if entry.IsDir() {
-		// ensure there is one and only one trailng slash on dirs
-		sep := string(os.PathSeparator)
-		path = strings.TrimRight(path, sep) + sep
-		// just make a note of this dir for now
-		a.dirmap[path] = ""
-	} else if entry.Mode().IsRegular() {
-		// hash the file contents
-		e := a.hashfile(path)
-		if e != nil {
-			return e
+	for _, rootdir := range a.rootdirs {
+		rootpath := rootdir.Name
+		if rootpath == path {
+			// already added the root level dirs
+			return nil
 		}
-	} else {
-		return errors.New("Only works on regular files: " + path)
+		// which rootpath are we in?
+		if rootpath == path[:len(rootpath)] {
+			trimpath := path[len(rootpath)+1:]
+			segments := strings.Split(trimpath, string(os.PathSeparator))
+			if entry.IsDir() {
+				return rootdir.addDir(segments)
+			}
+			if !entry.Mode().IsRegular() {
+				return errors.New("irregular files not handled: " + path)
+			}
+			// must be a file:
+			//fmt.Println("adding a file " + path)
+			return rootdir.addFile(segments)
+		}
 	}
-	return nil
+	return errors.New("path outside requested roots: " + path)
 }
 
 func main() {
@@ -135,5 +193,8 @@ func main() {
 	if err != nil {
 	    log.Println(err)
 	}
-	log.Println(a.requestedpaths)
+	for _, v := range a.rootdirs {
+		fmt.Println()
+		v.Dump("")
+	}
 }
